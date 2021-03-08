3209676b8846ab0305367bd0902a03816187b345
maquino96 <matthewaquino96@gmail.com>
2021-03-08 16:04:16 -0500

    basic skeleton

--- src/index.js
+++ src/index.js
@@ -2,6 +2,66 @@
 
 const div = document.querySelector('div#button-div')
 const url = 'http://localhost:3000'
+const login = document.querySelector('form#login-form')
+const signup = document.querySelector('form#signup-form')
+const task = document.querySelector('form#task-form')
+
+
+task.style.display = 'none'
+
+login.addEventListener('submit', event => {
+    event.preventDefault()
+
+    fetch(`${url}/users/login`, {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json',
+                        'Accept': 'application/json'},
+        body: JSON.stringify({name: event.target.name.value}) 
+    })
+        .then(r => r.json())
+        .then(user => {
+            login.dataset.id = user.id
+            task.style.display = 'block'
+            console.log(user)})
+        .catch( error => console.log(error))
+
+})
+
+signup.addEventListener('submit', event => {
+    event.preventDefault()
+
+    fetch(`${url}/users`, {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json',
+                        'Accept': 'application/json'},
+        body: JSON.stringify({name: event.target.name.value}) 
+    })
+        .then(r => r.json())
+        .then(user => {
+            console.log(user)
+        })
+        .catch( error => console.log(error))
+
+})
+
+task.addEventListener('submit', event => {
+    event.preventDefault()
+
+    fetch(`${url}/tasks`, {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json',
+                        'Accept': 'application/json'},
+        body: JSON.stringify({
+                name: event.target.name.value,
+                user_id: login.dataset.id}) 
+    })
+        .then(r => r.json())
+        .then(task => {
+            console.log(task)
+        })
+        .catch( error => console.log(error))
+})
+
 
 
 
@@ -13,7 +73,7 @@ div.addEventListener('click', event => {
             method: 'POST',
             headers: {'Content-Type' : 'application/json',
                         'Accept': 'application/json'},
-            body: JSON.stringify({user_id: 1}) 
+            body: JSON.stringify({user_id: login.dataset.id}) 
         })
         .then(r => r.json())
         .then( studySession => {
